56d70d0c3a6e2e93e7d554aacda6dd9d407fd2a9
papillon_nebula <Papillon-Pebula@outlook.com>
2021-01-18 23:44:42 +0800

    mysql链接

--- sql.py
+++ sql.py
@@ -7,15 +7,17 @@ from sqlalchemy.engine.base import Engine
 
 
 engine = create_engine(
-    "mysql+pymysql://debian-sys-maint:NbiTAGtSBbVbjyNI@1.203.144.58:3306/test?charset=utf8",
+    "mysql+pymysql://debian-sys-maint:NbiTAGtSBbVbjyNI@127.0.0.1:3306/nebula?charset=utf8",
     max_overflow=0,
     pool_size=5,
     pool_timeout=30,
     pool_recycle=-1
 )
 
-conn=engine.row_connection()
+conn=engine.raw_connection()
 
 cursor = conn.cursor()
 
-cursor.excute('select * from ')
+cursor.execute('select * from Nebula')
+res = cursor.fetchall()
+print(res)
